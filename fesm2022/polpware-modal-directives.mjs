ace7f52b631246b5e9919af8e3a93462a2879e95
Tomcat <polpware@gmail.com>
2025-07-11 22:03:38 -0400

    New publish

--- fesm2022/polpware-modal-directives.mjs
+++ fesm2022/polpware-modal-directives.mjs
@@ -51,52 +51,46 @@ class polpModalDraggableDirective {
     onMouseLeave(event) {
         this.isDraggable = false;
     }
-}
-polpModalDraggableDirective.ɵfac = function polpModalDraggableDirective_Factory(t) { return new (t || polpModalDraggableDirective)(i0.ɵɵdirectiveInject(i0.ElementRef)); };
-polpModalDraggableDirective.ɵdir = /*@__PURE__*/ i0.ɵɵdefineDirective({ type: polpModalDraggableDirective, selectors: [["", "polpModalDraggable", ""]], hostBindings: function polpModalDraggableDirective_HostBindings(rf, ctx) {
-        if (rf & 1) {
+    static { this.ɵfac = function polpModalDraggableDirective_Factory(t) { return new (t || polpModalDraggableDirective)(i0.ɵɵdirectiveInject(i0.ElementRef)); }; }
+    static { this.ɵdir = /*@__PURE__*/ i0.ɵɵdefineDirective({ type: polpModalDraggableDirective, selectors: [["", "polpModalDraggable", ""]], hostBindings: function polpModalDraggableDirective_HostBindings(rf, ctx) { if (rf & 1) {
             i0.ɵɵlistener("mousedown", function polpModalDraggableDirective_mousedown_HostBindingHandler($event) { return ctx.onMouseDown($event); })("mouseup", function polpModalDraggableDirective_mouseup_HostBindingHandler($event) { return ctx.onMouseUp($event); }, false, i0.ɵɵresolveDocument)("mousemove", function polpModalDraggableDirective_mousemove_HostBindingHandler($event) { return ctx.onMouseMove($event); }, false, i0.ɵɵresolveDocument)("mouseleave", function polpModalDraggableDirective_mouseleave_HostBindingHandler($event) { return ctx.onMouseLeave($event); }, false, i0.ɵɵresolveDocument);
-        }
-    } });
-(function () {
-    (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(polpModalDraggableDirective, [{
-            type: Directive,
-            args: [{
-                    selector: '[polpModalDraggable]'
-                }]
-        }], function () { return [{ type: i0.ElementRef }]; }, { onMouseDown: [{
-                type: HostListener,
-                args: ['mousedown', ['$event']]
-            }], onMouseUp: [{
-                type: HostListener,
-                args: ['document:mouseup', ['$event']]
-            }], onMouseMove: [{
-                type: HostListener,
-                args: ['document:mousemove', ['$event']]
-            }], onMouseLeave: [{
-                type: HostListener,
-                args: ['document:mouseleave', ['$event']]
-            }] });
-})();
+        } } }); }
+}
+(function () { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(polpModalDraggableDirective, [{
+        type: Directive,
+        args: [{
+                selector: '[polpModalDraggable]'
+            }]
+    }], function () { return [{ type: i0.ElementRef }]; }, { onMouseDown: [{
+            type: HostListener,
+            args: ['mousedown', ['$event']]
+        }], onMouseUp: [{
+            type: HostListener,
+            args: ['document:mouseup', ['$event']]
+        }], onMouseMove: [{
+            type: HostListener,
+            args: ['document:mousemove', ['$event']]
+        }], onMouseLeave: [{
+            type: HostListener,
+            args: ['document:mouseleave', ['$event']]
+        }] }); })();
 
 class PolpDraggableModule {
+    static { this.ɵfac = function PolpDraggableModule_Factory(t) { return new (t || PolpDraggableModule)(); }; }
+    static { this.ɵmod = /*@__PURE__*/ i0.ɵɵdefineNgModule({ type: PolpDraggableModule }); }
+    static { this.ɵinj = /*@__PURE__*/ i0.ɵɵdefineInjector({}); }
 }
-PolpDraggableModule.ɵfac = function PolpDraggableModule_Factory(t) { return new (t || PolpDraggableModule)(); };
-PolpDraggableModule.ɵmod = /*@__PURE__*/ i0.ɵɵdefineNgModule({ type: PolpDraggableModule });
-PolpDraggableModule.ɵinj = /*@__PURE__*/ i0.ɵɵdefineInjector({});
-(function () {
-    (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(PolpDraggableModule, [{
-            type: NgModule,
-            args: [{
-                    declarations: [
-                        polpModalDraggableDirective
-                    ],
-                    exports: [
-                        polpModalDraggableDirective
-                    ]
-                }]
-        }], null, null);
-})();
+(function () { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(PolpDraggableModule, [{
+        type: NgModule,
+        args: [{
+                declarations: [
+                    polpModalDraggableDirective
+                ],
+                exports: [
+                    polpModalDraggableDirective
+                ]
+            }]
+    }], null, null); })();
 (function () { (typeof ngJitMode === "undefined" || ngJitMode) && i0.ɵɵsetNgModuleScope(PolpDraggableModule, { declarations: [polpModalDraggableDirective], exports: [polpModalDraggableDirective] }); })();
 
 /*
